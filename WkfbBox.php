f0f68b75cfc09635d001fb93055be46c19703c2b
Adrian Korzan <adrian.korzan@gmail.com>
2020-03-02 23:28:37 +0100

    (feature): Tworzenie metody frontView

--- WkfbBox.php
+++ WkfbBox.php
@@ -30,11 +30,91 @@
 		private $buttonUri = '';
 
 		public $output = '';
+
 		/**
 		* Constructor of class.
 		*/
 		public function __construct($config) {
 			$this->cf = $config;
+		}
+
+		/**
+		 * Box Templpate
+		 */
+		public function frontView(): Void
+		{
+			$page_url = 'https://www.facebook.com/'.$this->cf['url'];
+			$this->output .= '<div class="fb-root"></div>
+			<script>(function(d, s, id) {
+					var js, fjs = d.getElementsByTagName(s)[0];
+					if (d.getElementById(id)) return;
+					js = d.createElement(s); js.id = id;
+					js.src = "//connect.facebook.net/'.$this->cf['locale'].'/sdk.js#xfbml=1&version=v3.3&appId=339779149790099";
+					fjs.parentNode.insertBefore(js, fjs);
+			}(document, \'script\', \'facebook-jssdk\'));</script>
+			<style type="text/css">' . $this->smallScreensStyle().' .fb-xfbml-parse-ignore {
+							display: none;
+					}
+					
+					.aspexifblikebox {
+							overflow: hidden;
+							z-index: 99999999;
+							position: fixed;
+							padding: '.$this->cssPlacement[1].';
+							top: ' . $this->cssPlacement[2] . ';
+							right: -' . ($this->cf['width']) . 'px;
+					}
+					
+					.aspexifblikebox .aspexi_facebook_iframe {
+							padding: 0;
+							border: ' . $borderwidth . 'px solid ' . $bordercolor . ';
+							background: #fff;
+							width: ' . $width . 'px;
+							height: ' . $height . 'px;
+							box-sizing: border-box;
+					}
+					
+					.aspexifblikebox .fb-page {
+							background: url("' . WKFBOX_URL . 'images/load.gif") no-repeat center center;
+							width: ' . ($width - ($borderwidth * 2)). 'px;
+							height: ' . ($height - ($borderwidth * 2)). 'px;
+							margin: 0;
+					}
+					
+					.aspexifblikebox .fb-page span {
+							background: #fff;
+							height: 100% !important;
+					}
+					
+					.aspexifblikebox .aspexi_facebook_button {
+							background: url("' . $button_uri . '") no-repeat scroll transparent;
+							height: '.$buttonHeight.'px;
+							width: 48px;
+							position: absolute;
+							'.$wkfbFunctions->setIconVertical($iconVertical, $this->cf['iconVerticalConst']).'
+							left: 0;
+							cursor: pointer;
+					}
+			</style>
+			<div class="aspexifblikebox">
+					<div class="aspexi_facebook_button"></div>
+					<div class="aspexi_facebook_iframe">
+							<div 
+								class="fb-page" 
+								data-href="'.$page_url.'" 
+								data-hide-cta="'.$hideCta.'"
+								data-width="'.($width - 4).'" 
+								data-height="'.($height - 4).'" 
+								data-hide-cover="'.$hideCover.'"
+								data-show-posts="'.$showPosts.'" 
+								data-show-facepile="'.$friendsFaces.'" 
+								data-adapt-container-width="'.$adaptative.'"
+								data-small-header="'.$smallHeader.'"
+								data-tabs="'.$timeLine.$messages.$events.'"
+							>
+							<div class="fb-xfbml-parse-ignore"><blockquote cite="'.$page_url.'"><a href="'.$page_url.'">Facebook</a></blockquote></div></div>
+					</div>
+			</div>';
 		}
 			
 		/**
@@ -43,7 +123,7 @@
 		* @param Int $distance
 		* @return String
 		*/
-		public function setIconVertical(String $iconVertical = '', Int $distance = 0): String
+		private function setIconVertical(String $iconVertical = '', Int $distance = 0): String
 		{
 			if ($iconVertical == "middle") $iconVertical = "top: 50%; transform: translateY(-50%);";
 			if ($iconVertical == "fixed"){
@@ -54,7 +134,11 @@
 			return $iconVertical;
 		}
 
-		public function smallScreensesStyle()
+		/**
+		* Set style for small devices
+		* @return String
+		*/
+		public function smallScreensStyle(): String
 		{
 			if( $this->cf['width'] > 0 ) {
 				$this->cf['borderWidth'] = 2; //temp
@@ -63,6 +147,9 @@
 		}
 		}
 
+		/**
+	 	* set placement of box. 
+		*/
 		public function setCssPlacement(): void
 		{
 			if( 'left' == $this->placement ) {
@@ -87,12 +174,10 @@
 		/**
 		* Set Button Uri
 		*/
-		private function setButtonUri()
+		private function setButtonUri(): void
 		{
 			if( file_exists( plugin_dir_path( __FILE__ ).'images/'.$this->cf['fbIcon'] ) ) $this->buttonUri = WKFBOX_URL.'images/'.$this->cf['fbIcon'];
-			else if( '' == $this->buttonUri ) {
-				$this->buttonUri = WKFBOX_URL.'images/fb1-right.png' ;
-			}
+			else if( '' == $this->buttonUri ) $this->buttonUri = WKFBOX_URL.'images/fb1-right.png' ;
 		}
 
 		
